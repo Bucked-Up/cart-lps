6568e75aad5181502061454e3286430b80d27213
deigo-lps <diego.lopes0909@gmail.com>
2025-06-23 17:45:22 -0300

    changes us store to funnels.buckedup.com

--- src/js/modules/handleBuy.js
+++ src/js/modules/handleBuy.js
@@ -35,6 +35,7 @@ const handleBuy = (country) => {
   sendVibeLead();
   toggleLoading();
   if (country === "uk") window.location.href = `https://www.buckedup.co.uk/cart/add?${string}&clear=true&${urlParams}`;
+  else if (country === "us") window.location.href = `https://funnels.buckedup.com/cart/add?${string}&clear=true&${urlParams}`;
   else window.location.href = `https://${country && country !== "us" ? country + "." : ""}buckedup.com/cart/add?${string}&clear=true&${urlParams}`;
   // else console.log(`https://${country && country !== "us" ? country + "." : ""}buckedup.com/cart/add?${string}&clear=true&${urlParams}`);
 };
