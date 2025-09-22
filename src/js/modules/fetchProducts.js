f708ab136915ff6c84852724a7f200cf036916bd
deigo-lps <diego.lopes0909@gmail.com>
2025-09-22 14:53:25 -0300

    fixes fetch from other countries

--- src/js/modules/fetchProducts.js
+++ src/js/modules/fetchProducts.js
@@ -54,7 +54,9 @@ const fetchProducts = async ({ country, productIds }) => {
   const fetchApi = async (id) => {
     let url = `https://funnels.buckedup.com/product/json/detail?product_id=${id}`;
     // let url = `https://webhook-processor-production-4aa3.up.railway.app/webhook/dev?product_id=${id}`;
-    if (country && country === "us-main") url = `https://www.buckedup.com/product/json/detail?product_id=${id}`
+    if (country === "us-main") url = `https://www.buckedup.com/product/json/detail?product_id=${id}`;
+    else if (country === "uk") url = `https://www.buckedup.co.uk/product/json/detail?product_id=${id}`;
+    else if (country && country !== "us") url = `https://${country}.buckedup.com/product/json/detail?product_id=${id}`;
     // if (country && country === "us-main") url = `https://webhook-processor-production-4aa3.up.railway.app/webhook/dev-us-main?product_id=${id}`
     try {
       const response = await fetch(url);
