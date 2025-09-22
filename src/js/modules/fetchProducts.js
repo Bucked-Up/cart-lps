d394087ac3714b5d43163e553b9b62e68f9833b5
deigo-lps <diego.lopes0909@gmail.com>
2025-09-22 13:45:07 -0300

    adds www to us-main

--- src/js/modules/fetchProducts.js
+++ src/js/modules/fetchProducts.js
@@ -54,7 +54,7 @@ const fetchProducts = async ({ country, productIds }) => {
   const fetchApi = async (id) => {
     let url = `https://funnels.buckedup.com/product/json/detail?product_id=${id}`;
     // let url = `https://webhook-processor-production-4aa3.up.railway.app/webhook/dev?product_id=${id}`;
-    if (country && country === "us-main") url = `https://buckedup.com/product/json/detail?product_id=${id}`
+    if (country && country === "us-main") url = `https://www.buckedup.com/product/json/detail?product_id=${id}`
     try {
       const response = await fetch(url);
       if (response.status === 404) throw new Error(`Product ${id} Not Found.`);
